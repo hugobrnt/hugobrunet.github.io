3469982c1da77296591defbaaa108c408a601da9
hugobrnt <go.brunet@gmail.com>
2026-06-15 17:01:29 +0200

    Changed astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import remarkMath from 'remark-math';   // lets you write $...$ math in Markdown
 import rehypeKatex from 'rehype-katex'; // renders that math to HTML at build time
 
 export default defineConfig({
-  // TODO: change this to your real URL, e.g. 'https://yourusername.github.io'
-  site: 'https://hugobrunet.github.io',
+  site: 'https://hugobrnt.github.io',
 
   markdown: {
     remarkPlugins: [remarkMath],
